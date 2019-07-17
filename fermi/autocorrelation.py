644c318d24f2ac8fa6aeb9a93bcd22e308e3b504
Michael Korsmeier <korsmeier@physik.rwth-aachen.de>
2019-07-18 01:43:29 +0200

    New simple models for Cp: 1) changed PWL model. Normalize dN/dS now at S0=1e-10 cm2s-1. 2) PWL model with Gamma distribution

--- fermi/autocorrelation.py
+++ fermi/autocorrelation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from scipy import integrate
 
 import MKastro.fermi.observables        as fermiObs
 
@@ -35,19 +36,21 @@ get_ij = np.vectorize(_get_ij)
 
 
 
-######### Functions for simple Cp model
-#
-#   dN_ph/dE ~ E^-Gamma
-#   dN/dS    = A S^-beta
-#
-#
-
 def powerlaw_integral(xmin, xmax, alpha):
     if np.fabs(1+alpha) < 1e-5:
         return np.ln( xmax/xmin )
     return 1./(1+alpha) * ( np.power( xmax, 1+alpha ) - np.power( xmin, 1+alpha ) )
 
+######### Functions for simple Cp model
+#
+#   dN_ph/dE ~ E^-Gamma
+#   dN/dS    = A (S/S_0)^-beta
+#
+#      S: Flux in the energy intervall 1GV - 100 GeV
+#      S_0 = 1e-10 cm^2 s^-1
+#
 def _get_Cp_simple_model( Emin, Emax, E2min, E2max,  A, beta, Gamma, k=1.  ):
+    S_0 = 1e-10
     F1000_cut_4FGL = fermiObs.F_thresh__Gamma_4FGL( Gamma )
     F1000_cut_3FHL = fermiObs.F_thresh__Gamma_3FHL( Gamma )
     
@@ -69,6 +72,20 @@ def _get_Cp_simple_model( Emin, Emax, E2min, E2max,  A, beta, Gamma, k=1.  ):
     integral_E2 = powerlaw_integral(  E2min, E2max,         -Gamma       )
     integral_E  = powerlaw_integral(  1.,    100. ,         -Gamma       )
     integral_F  = powerlaw_integral(  0.,    F_cut*k,       -beta+2    )
-    return A*integral_E1*integral_E2/integral_E**2 * integral_F
-
+    return A/S_0**-beta * integral_E1*integral_E2/integral_E**2 * integral_F
 get_Cp_simple_model = np.vectorize(_get_Cp_simple_model)
+
+
+######### Functions for simple Cp model
+#
+#   dN_ph/dE ~ E^-Gamma
+#   dN/dS    = A (S/S_0)^-beta * 1/sqrt(2pi)/sigma * exp( - (Gamma-mu)^2/2/sigam^2 )
+#
+#      S: Flux in the energy intervall 1GV - 100 GeV
+#      S_0 = 1e-10 cm^2 s^-1
+#
+def _get_Cp_simple_model_GammaDistr( Emin, Emax, E2min, E2max,  A, beta, mu, sigma, k=1.  ):
+    Gamma_min = 1.0
+    Gamma_max = 3.0
+    return integrate.quad(lambda Gamma: _get_Cp_simple_model(Emin, Emax, E2min, E2max,  A, Gamma=Gamma, beta=beta, k=k)*1./(sigma * np.sqrt(2 * np.pi)) * np.exp( - (Gamma - mu)**2 / (2 * sigma**2) ), Gamma_min, Gamma_max)[0]
+get_Cp_simple_model_GammaDistr = np.vectorize(_get_Cp_simple_model_GammaDistr)
